c3492ac767e9a2781dcb9e8327207ac81aad73bb
Aehlius <aehlius@gmail.com>
2020-05-30 14:19:20 -0400

    Game Can Now Draw Cards Basically, the deck is now stored in an array, and both players' hands are stored in arrays as well. There's a component called Card that just draws a card, and two other components that draws my hand and the other player's hand.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css'
+
+
+var p1Deck;
+var p2Deck;
+
+var deck = ["As", "Ah", "Ad", "Ac", "6s", "6h", "6d", "6c", "7s", "7h", "7d", "7c", "8s", "8h", "8d", "8c", "9s", "9h", "9d", "9c", "10s", "10h", "10d", "10c", "Js", "Jh", "Jd", "Jc", "Qs", "Qh", "Qd", "Qc", "Ks", "Kh", "Kd", "Kc"];
+
+function Card(props) {
+    if(props.player === 'myCard') {
+        return <button className={"card myCard"}>{props.value}</button>;
+    } else if(props.player === 'otherCard'){
+        return <button className={"card otherCard"}>{props.value}</button>;
+    }
+}
+
+var myHand = ["As", "Ah", "Ad", "Ac", "6s", "6h"];
+
+var otherHand = ["As", "Ah", "Ad", "Ac", "6s", "6h"];
+
+
+function DrawMyHand(props){
+    var handToReturn = [];
+    for(var i = 0; i < props.hand.length; i++){
+        handToReturn.push(<Card value={props.hand[i]} player={'myCard'} />);
+    }
+
+    return handToReturn;
+}
+
+function DrawOtherHand(props){
+    var handToReturn = [];
+    for(var i = 0; i < props.hand.length; i++){
+        handToReturn.push(<Card value={props.hand[i]} player={'otherCard'} />);
+    }
+
+    return handToReturn;
+}
 
 
 class App extends React.Component {
 
   render(){
-    return <h1>Hello, Durak!</h1>;
+    return (
+        <div>
+            <h1>Hello, Durak!</h1>
+            <div id={"otherHand"}>
+                <DrawOtherHand hand={otherHand} />
+            </div>
+            <div id={"myHand"}>
+                <DrawMyHand hand={myHand} />
+            </div>
+        </div>
+    );
   }
 }
 
+
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
