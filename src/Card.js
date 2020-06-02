5dc024200ecd599c2d8acfddaf46e8142d3035ad
Aehlius <aehlius@gmail.com>
2020-06-02 10:36:53 -0400

    Moved card drawing components to Card.js Basically moved anything that involved the card's components to Card.js, including the function to move cards to the table

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,26 @@
+import React from 'react';
+import * as main from './index.js';
+
 export class Card extends React.Component {
+    constructor(props) {
+        super(props);
+        this.addToTable = this.addToTable.bind(this);
+    }
+
+    addToTable(){
+        if(this.props.color === 'myCard') {
+            moveCard(this.props.myHand, this.props.table, this.props.myHand.indexOf(this.props.value))
+            main.renderDom();
+        }
+    }
 
     render() {
-        if ('myCard' === this.props.player) {
-            return <button className={"card myCard"} >{this.props.value}</button>;
-        } else if (this.props.player === 'otherCard') {
+        if (this.props.color === 'myCard') {
+            return <button className={"card myCard"} onClick={this.addToTable}>{this.props.value}</button>;
+        } else if (this.props.color === 'otherCard') {
             return <button className={"card otherCard"}>{this.props.value}</button>;
-        } else if (this.props.player === 'table') {
-            return <button className={"card"}>{this.props.value}</button>;
+        } else if (this.props.color === 'table') {
+            return <button className={"card table"}>{this.props.value}</button>;
         } else {
             return <button className={"card trumpCard"}>{this.props.value}</button>;
         }
@@ -18,7 +32,7 @@ export class Card extends React.Component {
 export function DrawMyHand(props){
     var handToReturn = [];
     for(var i = 0; i < props.hand.length; i++){
-        handToReturn.push(<Card value={props.hand[i]} player={'myCard'} />);
+        handToReturn.push(<Card table = {props.table} myHand={props.hand} value={props.hand[i]} color={'myCard'} />);
     }
 
     return handToReturn;
@@ -27,20 +41,28 @@ export function DrawMyHand(props){
 export function DrawOtherHand(props){
     var handToReturn = [];
     for(var i = 0; i < props.hand.length; i++){
-        handToReturn.push(<Card value={props.hand[i]} player={'otherCard'} />);
+        handToReturn.push(<Card value={props.hand[i]} table = {props.table} myHand={props.myHand} color={'otherCard'} />);
     }
 
     return handToReturn;
 }
 
-export function DrawTable(props){
-    var tableCardsToDraw = [];
-
-    for(var i  = 0; i < props.tableCards.length; i++){
-        tableCardsToDraw.push( <Card value={props.tableCards[i]} player={props.player}/>)
+export class DrawMyTable extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {table: this.props.table};
     }
 
-    return tableCardsToDraw;
+    render(){
+        var tableCardsToDraw = [];
+
+        for (var i = 0; i < this.state.table.length; i++) {
+            tableCardsToDraw.push(<Card value={this.state.table[i]} table = {this.props.table} myHand={this.state.table} color={this.props.color}/>)
+        }
+
+        return tableCardsToDraw;
+
+    }
 }
 
 export function moveCard(initArray, finalArray, cardIndex){
@@ -54,3 +76,4 @@ export function setupCards(deck, hand, numberOfCards){
     }
 
 }
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import * as card from './Card';
 
 var deck = ["As", "Ah", "Ad", "Ac", "6s", "6h", "6d", "6c", "7s", "7h", "7d", "7c", "8s", "8h", "8d", "8c", "9s", "9h", "9d", "9c", "10s", "10h", "10d", "10c", "Js", "Jh", "Jd", "Jc", "Qs", "Qh", "Qd", "Qc", "Ks", "Kh", "Kd", "Kc"];
 var myHand = [];
@@ -8,9 +9,9 @@ var otherHand = [];
 var table = [];
 var trump = [];
 
-setupCards(deck, myHand, 6);
-setupCards(deck, otherHand, 6);
-setupCards(deck, trump, 1);
+card.setupCards(deck, myHand, 6);
+card.setupCards(deck, otherHand, 6);
+card.setupCards(deck, trump, 1);
 
 
 
@@ -21,18 +22,18 @@ class App extends React.Component {f
         <div>
             <h1>Durak</h1>
             <div id={"otherHand"}>
-                <DrawOtherHand hand={otherHand} />
+                <card.DrawOtherHand hand={otherHand} />
             </div>
             <div id={"OtherTable"}>
             </div>
             <div id={"MyTable"}>
-                <DrawMyTable color={'table'}/>
+                <card.DrawMyTable table={table} color={'table'}/>
             </div>
             <div id={"myHand"}>
-                <DrawMyHand hand={myHand} color={'myCard'} />
+                <card.DrawMyHand table={table} hand={myHand} color={'myCard'} />
             </div>
             <div id={"trumpSuit"}>
-                <Card value={trump[0]} />
+                <card.Card value={trump[0]} />
             </div>
         </div>
     );
@@ -40,85 +41,7 @@ class App extends React.Component {f
 }
 
 
-export class Card extends React.Component {
-    constructor(props) {
-        super(props);
-        this.addToTable = this.addToTable.bind(this);
-    }
-
-    addToTable(){
-        if(this.props.color === 'myCard') {
-            moveCard(myHand, table, myHand.indexOf(this.props.value))
-            renderDom();
-        }
-    }
-
-    render() {
-        if (this.props.color === 'myCard') {
-            return <button className={"card myCard"} onClick={this.addToTable}>{this.props.value}</button>;
-        } else if (this.props.color === 'otherCard') {
-            return <button className={"card otherCard"}>{this.props.value}</button>;
-        } else if (this.props.color === 'table') {
-            return <button className={"card table"}>{this.props.value}</button>;
-        } else {
-            return <button className={"card trumpCard"}>{this.props.value}</button>;
-        }
-    }
-}
-
-
-
-function DrawMyHand(props){
-    var handToReturn = [];
-    for(var i = 0; i < props.hand.length; i++){
-        handToReturn.push(<Card value={props.hand[i]} color={'myCard'} />);
-    }
-
-    return handToReturn;
-}
-
-function DrawOtherHand(props){
-    var handToReturn = [];
-    for(var i = 0; i < props.hand.length; i++){
-        handToReturn.push(<Card value={props.hand[i]} color={'otherCard'} />);
-    }
-
-    return handToReturn;
-}
-
-class DrawMyTable extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {table: table};
-    }
-
-    render(){
-        var tableCardsToDraw = [];
-
-        for (var i = 0; i < this.state.table.length; i++) {
-            tableCardsToDraw.push(<Card value={this.state.table[i]} color={this.props.color}/>)
-        }
-
-        return tableCardsToDraw;
-
-    }
-}
-
-function moveCard(initArray, finalArray, cardIndex){
-    finalArray.push(initArray[cardIndex]);
-    initArray.splice(cardIndex, 1)
-}
-
-function setupCards(deck, hand, numberOfCards){
-    for(var i = hand.length; i < numberOfCards; i ++){
-        moveCard(deck, hand, Math.floor(Math.random() * deck.length))
-    }
-
-}
-
-
-
-function renderDom(){
+export function renderDom(){
     ReactDOM.render(
         <React.StrictMode>
             <App />
